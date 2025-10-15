71225c5f7db41fe9330562a3aea65c052a6617af
Ramon Sibello <ramon@sibello.ca>
2025-10-15 17:00:58 -0400

    Add more known_bugs tests for scattered @todos

--- src/jolpica/tests/known_bugs.rs
+++ src/jolpica/tests/known_bugs.rs
@@ -5,11 +5,24 @@
 //! the underlying issue has been fixed, and the associated workaround can be removed.
 
 mod tests {
+    use std::sync::LazyLock;
+
     use pretty_assertions::assert_eq;
 
-    use crate::jolpica::{agent::Agent, resource::Filters, response::RaceResult};
+    use crate::{
+        error::Error,
+        jolpica::{
+            agent::Agent,
+            resource::Filters,
+            response::{Position, QualifyingResult, RaceResult},
+            time::{QualifyingTime, duration_m_s_ms},
+        },
+    };
 
     use crate::jolpica::tests::assets::*;
+
+    /// Shared instance of [`Agent`] for use in tests, to share a rate limiter, cache, etc.
+    static JOLPICA: LazyLock<Agent> = LazyLock::new(|| Agent::default());
 
     #[test]
     fn race_result_buggy_time() {
@@ -27,11 +40,58 @@ mod tests {
         // I don't understand what this is. For now, as a workaround, those values are being parsed
         // as None for the race time field. If/when this test fails, we can investigate further.
         assert_eq!(
-            Agent::default()
+            JOLPICA
                 .get_race_result(Filters::new().season(2023).round(3).finish_pos(15))
                 .unwrap()
                 .race_result(),
             &*RACE_RESULT_2023_3_P15
         );
+    }
+
+    #[test]
+    fn qualifying_result_2023_4_p3() {
+        // @todo jolpica-f1 is incorrectly reporting Q1 time as 1:41.131, but it should be 1:41.756
+        assert_eq!(QUALIFYING_RESULT_2023_4_P3.q1, Some(QualifyingTime::Time(duration_m_s_ms(1, 41, 131))));
+        assert_eq!(
+            serde_json::from_str::<QualifyingResult>(QUALIFYING_RESULT_2023_4_P3_STR).unwrap(),
+            *QUALIFYING_RESULT_2023_4_P3
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn get_qualifying_result_2023_4_p3() {
+        assert_eq!(
+            JOLPICA
+                .get_qualifying_result(Filters::new().season(2023).round(4).driver_id("perez".into()))
+                .unwrap()
+                .qualifying_result(),
+            &*QUALIFYING_RESULT_2023_4_P3
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn qualifying_pos_filter() {
+        // @todo [`Filters::qualifying_pos`] appears to not be functional in the new jolpica-f1 API
+        // If/when this test begins to fail, and we can add tests filtering by `qualifying_pos`
+        assert!(matches!(
+            JOLPICA.get_qualifying_result(Filters::new().season(2023).round(4).qualifying_pos(1)),
+            Err(Error::TooMany)
+        ));
+    }
+
+    #[test]
+    #[ignore]
+    fn finish_pos_filter_for_non_finishing() {
+        // @todo Counterintuitively, non-finishing race results cannot be filtered by .finish_pos,
+        // even though .position would be set. Is it only meant to filter by Position::Finished(_)?
+        assert_eq!(RACE_RESULT_2023_4_P20.position, 20);
+        assert_eq!(RACE_RESULT_2023_4_P20.position_text, Position::R);
+
+        assert!(matches!(
+            JOLPICA.get_race_result(Filters::new().season(2023).round(4).finish_pos(20)),
+            Err(Error::NotFound)
+        ));
     }
 }
